ed6d8688dc14c4ea3fb0612b9e030c4017d499af
Fausto Navadian <faustoagustin8@gmail.com>
2026-03-07 12:58:10 -0300

    Add weather icons

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,4 +1,13 @@
+import { weatherCodeMap } from "../services/weatherCodes";
+
 function WeatherCard({ weather }) {
+
+  const weatherInfo =
+    weatherCodeMap[weather.weathercode] || {
+      label: "Unknown",
+      icon: "❓"
+    };
+
   return (
     <div style={{
       marginTop: 20,
@@ -8,6 +17,13 @@ function WeatherCard({ weather }) {
       width: 250
     }}>
       <h2>{weather.city}</h2>
+
+      <h3 style={{ fontSize: 40 }}>
+        {weatherInfo.icon}
+      </h3>
+
+      <p>{weatherInfo.label}</p>
+
       <p>Temperature: {weather.temperature}°C</p>
       <p>Wind Speed: {weather.windspeed} km/h</p>
     </div>
---
+++ src/services/weatherCodes.js
@@ -0,0 +1,12 @@
+export const weatherCodeMap = {
+  0: { label: "Clear sky", icon: "☀️" },
+  1: { label: "Mainly clear", icon: "🌤️" },
+  2: { label: "Partly cloudy", icon: "⛅" },
+  3: { label: "Overcast", icon: "☁️" },
+  45: { label: "Fog", icon: "🌫️" },
+  48: { label: "Depositing rime fog", icon: "🌫️" },
+  51: { label: "Light drizzle", icon: "🌦️" },
+  61: { label: "Rain", icon: "🌧️" },
+  71: { label: "Snow", icon: "❄️" },
+  95: { label: "Thunderstorm", icon: "⛈️" }
+};
