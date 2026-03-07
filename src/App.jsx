947aa9b0c38a4fcc84bba6a7bf15bf98dd23309b
Fausto Navadian <faustoagustin8@gmail.com>
2026-03-07 11:55:40 -0300

    Add loading state

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import { getCoordinates, getWeather } from "./services/weatherService";
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const searchWeather = async () => {
     try {
+      setLoading(true);
+
       const coords = await getCoordinates(city);
       const weatherData = await getWeather(coords.latitude, coords.longitude);
+
       setWeather({ ...weatherData, city: coords.name });
     } catch (error) {
       alert(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -28,10 +34,12 @@ function App() {
 
       <button onClick={searchWeather}>Search</button>
 
-      {weather && (
+      {loading && <p>Loading weather...</p>}
+
+      {weather && !loading && (
         <div style={{ marginTop: 20 }}>
           <h2>{weather.city}</h2>
-          <p>Temperature: {weather.temperature}°C</p>
+          <p>Temperature: {weather.temperature} °C</p>
           <p>Wind Speed: {weather.windspeed} km/h</p>
         </div>
       )}
